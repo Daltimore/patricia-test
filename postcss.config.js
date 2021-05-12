943130820bf6c509f2d551ecc7fbe56dce549ac5
Daltimore <obinnaleo78@gmail.com>
2021-05-12 16:07:28 +0100

    initialize project

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+module.exports = {
+	"plugins": [
+ 		require('tailwindcss')('tailwind.config.js'),
+ 		require('autoprefixer')(),
+	]
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>This is the landing Page</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
